37cbe7ba4e5a3166be05c8b3c7d1887a7fdc9137
katehao <kate.hao@gmail.com>
2022-07-10 19:21:15 -0400

    finalizing content

--- scripts.js
+++ scripts.js
@@ -12,3 +12,19 @@ $(document).ready( function() {
     else { $("#returnLink").css({opacity: 0}); }
   });
 });
+
+//HBL image swaps
+$("#hblHead").click( function() {
+  var src = ($(this).attr("src")=="./images/HBL1_head.jpg")?"./images/HBL2_head.jpg":"./images/HBL1_head.jpg";
+  $(this).attr("src", src);
+});
+
+$("#hblBody").click( function() {
+  var src = ($(this).attr("src")=="./images/HBL1_body.jpg")?"./images/HBL2_body.jpg":"./images/HBL1_body.jpg";
+  $(this).attr("src", src);
+});
+
+$("#hblLegs").click( function() {
+  var src = ($(this).attr("src")=="./images/HBL1_legs.jpg")?"./images/HBL2_legs.jpg":"./images/HBL1_legs.jpg";
+  $(this).attr("src", src);
+});
